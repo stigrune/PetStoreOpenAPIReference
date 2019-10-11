18d632e2128eb1332b828ddb3b749e9a3613a694
Stig Rune <stigrune@gmail.com>
2019-10-11 20:38:08 +0200

    Add project files.

---
+++ PetStoreOpenAPIReference/ClientBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PetStore.Client
+{
+    public abstract class ClientBase
+    {
+        protected async Task<HttpRequestMessage> CreateHttpRequestMessageAsync(CancellationToken cancellationToken)
+        {
+            var msg = new HttpRequestMessage();
+            await Task.CompletedTask;
+            var token = "Get your token";
+            msg.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            return msg;
+        }
+    }
+}
